be06b904c2b5169b634fc76489894ef4da149ca6
Sy Dau <sdau@myseneca.ca>
2022-09-01 23:24:42 -0400

    update to correct files

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import {
-  Navbar,
-  VideoDetail,
-  SearchFeed,
-  ChannelDetail,
+  ChannelDetails,
   Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetails,
 } from './components';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Box sx={{ backgroundColor: '#000' }}>
+      <Box>
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Feed />} />
-          <Route path="/video/:id" exact element={<VideoDetail />} />
-          <Route path="/channel/:id" exact element={<ChannelDetail />} />
+          <Route path="/video/:id" exact element={<VideoDetails />} />
+          <Route path="/channel/:id" exact element={<ChannelDetails />} />
           <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
         </Routes>
       </Box>
